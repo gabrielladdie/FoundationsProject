05bddbcba8e226dcf7238d38ee1fc42814f0ac2d
Gabriell Yap <gabriell0414@gmail.com>
2024-09-19 17:00:50 -0400

    added comments on server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res, next) => { // res not used because middleware is only logging information about incoming requests; its not modifying or sending any response
     logger.info(`${req.method}: ${req.url} request received`);
     next();
 })
